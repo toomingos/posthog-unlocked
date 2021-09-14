d848511dcb4a9ec247a5b2ddc6a6112e260d8624
mariusandra <mariusandra@users.noreply.github.com>
2021-09-14 21:56:10 +0000

    Sync and remove all non-FOSS parts

--- ee/clickhouse/queries/__init__.py
+++
@@ -1,7 +0,0 @@
-from ee.clickhouse.queries.funnels.funnel import ClickhouseFunnel
-
-from .clickhouse_retention import ClickhouseRetention
-from .clickhouse_session_recording import SessionRecording
-from .clickhouse_stickiness import ClickhouseStickiness
-from .paths.paths import ClickhousePaths
-from .trends.clickhouse_trends import ClickhouseTrends
